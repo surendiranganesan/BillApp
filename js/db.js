6529f6b60ae1ec7e1699f5bf0750ff06b392ddde
sur90.gan@gmail.com <sur90.gan@gmail.com>
2017-01-01 20:46:38 +0530

    plugin install

--- js/db.js
+++ js/db.js
@@ -19,46 +19,70 @@ function successDB(error)
 
 /// Database Table creation script ends/////
 
+function edit(form,id)
+{
+	
+	console.log("#####"+form+"#####"+id);
+ 	database.transaction(function(tx) {
+    tx.executeSql('SELECT * FROM products where p_id=?', [id], function(tx, result) {
+    $.each(result.rows,function(index){
+    var row = result.rows.item(index);
+	document.getElementById("p_id").value=row['p_id'];
+	document.getElementById("code").value=row['code'];
+	document.getElementById("desc").value=row['desc'];
+	document.getElementById("unit").value=row['unit'];
+	document.getElementById("price").value=row['price'];
+	console.log(row);
+	$.mobile.navigate( "#addproduct" );
+
+	});
+	}, function(tx, error) {
+      console.log('SELECT error: ' + error.message);
+    });
+	}); 
+}
+
 
 function Prod_add_data(obj)
 {
 console.log(obj); 
 
-
 database.transaction(function(tx) {
+	if(obj.p_id === undefined ||  obj.p_id === null ||  obj.p_id === '')
     tx.executeSql('INSERT INTO products(code,desc,unit,price) VALUES (?,?,?,?)', [obj.code,obj.desc,obj.unit,obj.price]);
+	else
+    tx.executeSql('update products set code=?, desc=?, unit=?, price=? where p_id=?', [obj.code,obj.desc,obj.unit,obj.price,obj.p_id]);
   }, function(error) {
     console.log('Transaction ERROR: ' + error.message);
+	$("#status_msg").html('<span id="error_message" class="error">Transaction ERROR:<b>' + error.message+'</b></span>');
   }, function() {
-    console.log('Data added');
-	redirect('product');
-  });
-  
-  
-  
+	  $(".status_msg").fadeIn().html('<span id="success_message" class="success"><b>Data saved successfully</b></span>');
+				setTimeout(function() {
+					$('.status_msg').fadeOut("slow");
+				}, 2000 );
+	 $('#add_prod_form').trigger("reset");
+	 $('form#add_prod_form input[type=hidden]').val('');
+	 if(obj.p_id !== undefined &&  obj.p_id !== null &&  obj.p_id !== '')
+	 $.mobile.navigate( "#product" );
+
+  }); 
 }
 
 function Prod_select()
 {
-	console.log("kkk");
+	//console.log("kkk");
 	database.transaction(function(tx) {
-    tx.executeSql('SELECT * FROM products', [], function(tx, rs) {
-      var len = rs.rows.length, i;
-	//$("#rowCount").append(len);
-	for (i = 0; i < len; i++){
-		
-	//$("#prod_list_view").append('<li data-icon="check"><a href="">'+rs.rows.item(i).desc+'</a></li>');
-	$('ul#prod_list_view').append($('<li/>', {    //here appending `<li>`
-		'data-icon': "check"
-	}).append($('<a/>', {    //here appending `<a>` into `<li>`
-		'href': '#',
-		//'data-transition': 'slide',
-		'text': rs.rows.item(i).desc
-	})));
-	} 
-	$('ul#prod_list_view').listview('refresh');
+    tx.executeSql('SELECT * FROM products', [], function(tx, result) {
+	//	$('ul#prod_list_view').empty();
+		$('ul#prod_list_view').html('<li data-role="list-divider">Product</li>');
+        $.each(result.rows,function(index){
+            var row = result.rows.item(index);
+            $('ul#prod_list_view').append('<li><a href="#" onclick="edit(\'product\','+row['p_id']+');"><h3 class="ui-li-heading">'+row['desc']+'</h3><p class="ui-li-desc">Code: '+row['code']+'      '+'Unit: '+row['unit']+'      '+'Price: '+row['price']+'</p></a></li>');
+        });
+ 
+        $('ul#prod_list_view').listview('refresh');
     }, function(tx, error) {
       console.log('SELECT error: ' + error.message);
     });
   });
-}
+}				
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 var database=null;
+var bluetooth_prnt_var=null;
 (function ($) {
     $.fn.serializeFormJSON = function () {
 
@@ -17,24 +18,28 @@ var database=null;
         return o;
     };
 })(jQuery); 
+
+
 function redirect(page)
 {
 switch(page)
 {
 	case "product":
-	window.location="product.html";
+	$.mobile.navigate( "#product" );
 	break;
 	
 }	
 }
-$( document ).ready(function() {
-	
-document.addEventListener("deviceready",onDeviceReady,false);
+
 function save_prod(obj)
 {
 	console.log(obj);
-	Prod_add_data(obj);
+	Prod_add_data(obj); 
 }	
+
+$( document ).ready(function() {
+document.addEventListener("deviceready",onDeviceReady,false);
+
 $("#add_prod_form").validate({
 		errorPlacement: function (error, element) {
         error.appendTo(element.parent().prev());
@@ -45,6 +50,10 @@ $("#add_prod_form").validate({
         return false;	
 	}	  
 });
+
+$("#product").on("pageshow" , function() {
+  Prod_select();
+});
 });
 
 function onDeviceReady() 
@@ -53,9 +62,17 @@ function onDeviceReady()
 database=window.openDatabase("myappdb","1.0","Application Database",200000);
 database.transaction(PopulateDatabase,errorDB,successDB); 
 
+console.log(window);
+console.log(navigator);
+console.log(Camera);
+if (typeof window.BTPrinter !== 'undefined') {
+	console.log("inside");
+	bluetooth_prnt_var = BTPrinter;			
 }
 
-/* $(document).on('submit', '#add_prod_form', function (e) {
+}
+
+ $(document).on('submit', '#add_prod_form', function (e) {
 	
 	
     //cache the form element for use in this function
@@ -68,4 +85,49 @@ database.transaction(PopulateDatabase,errorDB,successDB);
 	
 	console.log($this.serialize());
 	
-}); */
+}); 
+
+
+function print_text(txt)
+{
+				BTPrinter.list(function(data){
+					console.log("Success");
+					alert("Success list");
+					console.log(data); 
+					var device_name=data[0];
+			  BTPrinter.connect(function(data){
+						console.log("Success connect");
+						alert("Success connect");
+						console.log(data);
+				BTPrinter.printText(function(data){
+				console.log("Success Print");
+				alert("Success Print");
+				console.log(data)
+				BTPrinter.disconnect(function(data){
+				console.log("Success disconnected");
+				alert("Success disconnected");
+				console.log(data)
+				},function(err){
+				console.log("Error");
+				alert("Error");
+				alert(err.toString());				
+				console.log(err)
+				}, device_name);
+				},function(err){
+				console.log("Error Print!");
+				alert("Error Print!");
+				alert(err.toString());
+				}, txt)			
+					},function(err){
+						console.log("Error");
+						alert("Error2");
+						alert(err.toString());
+						console.log(err)
+					}, device_name)
+			 },function(err){
+				 console.log("Error");
+				alert("Error 3");
+				alert(err.toString());
+				 console.log(err);
+			 })
+}
